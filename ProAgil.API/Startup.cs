663f3f95608c2299597fbfac351c6c71f409412b
alisson youssf <youssfbr@gmail.com>
2020-08-31 13:25:15 -0300

    Criando Data Context, .vscode no .gitignore

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProAgil.API.Data;
 
 namespace ProAgil.API
 {
@@ -25,6 +27,10 @@ namespace ProAgil.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("Connection"), builder =>
+                    builder.MigrationsAssembly("ProAgil.API")));
+
             services.AddControllers();
         }
 
